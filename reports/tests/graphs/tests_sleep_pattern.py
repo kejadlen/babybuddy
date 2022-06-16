3011b3135f2d568dd1e3cbc00c6fdcf65ef05d18
Alpha Chen <alpha@kejadlen.dev>
2022-06-16 13:12:25 -0700

    Add PBT for sleep_pattern

--- reports/tests/graphs/tests_sleep_pattern.py
+++ reports/tests/graphs/tests_sleep_pattern.py
@@ -1,37 +1,48 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
-from django.test import TestCase
 from django.utils import timezone
+
+from hypothesis import given, settings
+import hypothesis.strategies as st
+from hypothesis.extra.django import TestCase
 
 from core import models
 from reports.graphs import sleep_pattern
 
 
 class SleepPatternTestCase(TestCase):
-    def setUp(self):
-        self.original_tz = timezone.get_current_timezone()
-        self.tz = ZoneInfo("America/Los_Angeles")
-        timezone.activate(self.tz)
-
-    def tearDown(self):
-        timezone.activate(self.original_tz)
-
-    def test_sleep_pattern(self):
-
+    @given(
+            st.timezones(),
+            st.datetimes(timezones=st.timezones()),
+            st.lists(
+                st.tuples(
+                    st.integers(min_value=1, max_value=1000),
+                    st.integers(min_value=1, max_value=200),
+                ),
+                min_size=1,
+            )
+    )
+    @settings(deadline=None)
+    def test_fuzz_sleep_pattern(self, user_tz, start_dt, sleeps_raw):
         c = models.Child(birth_date=datetime.now())
         c.save()
 
-        s = models.Sleep.objects.create(
-            child=c,
-            start=datetime(2021, 8, 14, 19, 37, tzinfo=self.tz),
-            end=datetime(2021, 8, 14, 19, 53, tzinfo=self.tz),
-        )
-        s = models.Sleep.objects.create(
-            child=c,
-            start=datetime(2022, 6, 16, 19, 37, tzinfo=self.tz),
-            end=datetime(2022, 6, 16, 19, 53, tzinfo=self.tz),
-        )
+        original_tz = timezone.get_current_timezone()
+        timezone.activate(user_tz)
+
+        last_sleep_end = start_dt
+        for delta, duration in sleeps_raw:
+            start = last_sleep_end + timedelta(minutes=delta)
+            end = start + timedelta(minutes=duration)
+            last_sleep_end = end
+            models.Sleep.objects.create(
+                child=c,
+                start=start,
+                end=end,
+            )
 
         sleep_pattern(models.Sleep.objects.order_by("start"))
+
+        timezone.activate(original_tz)
